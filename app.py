5d40add3611929b23f898f6e31083a1422ae0a99
vsl366 <vishalthegreat366@gmail.com>
2025-04-11 20:53:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,4 +129,5 @@ def ocr_api():
         os.remove(pdf_path)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
